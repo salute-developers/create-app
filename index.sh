a7338ac146c51500c8ccc1088a6566eae4121145
Dmitriy Selyanin <selyanin.dmitriy@gmail.com>
2022-06-01 18:52:07 +0300

    salutejs/create-app сделать чтобы он работал на винде

--- index.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-SELF_PATH=$0
-LINK_PATH=$(readlink $SELF_PATH)
-LINK_DIR=$(dirname -- $LINK_PATH)
-PACKAGE_DIR=$(dirname -- $SELF_PATH)/$LINK_DIR
-RUN_DIR=$(pwd)
-
-node --experimental-import-meta-resolve $PACKAGE_DIR/src/cliWizard.mjs $@ --runDir $RUN_DIR
