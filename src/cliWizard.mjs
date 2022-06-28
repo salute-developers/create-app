e8714cb7be6b7d8fdfc15440e04ee8e250fd1915
rpg59 <vanxellshing24@gmail.com>
2022-06-28 20:09:18 +0300

    SPEED-393: Handle invalid `templatePath` flag

--- src/cliWizard.mjs
+++ src/cliWizard.mjs
@@ -6,7 +6,7 @@ import chalk from 'chalk';
 import { execSync } from 'child_process';
 import { existsSync } from 'fs';
 
-import { cleanUp, downloadTemplatePackage, extractTemplatePackage, getArgv, getDirname, printLogo } from './utils.mjs';
+import { cleanUp, downloadTemplatePackage, extractTemplatePackage, getArgv, printLogo } from './utils.mjs';
 import { buildTemplateTask } from './buildTemplate.mjs';
 import { getConfigFromAnswers } from './config.mjs';
 import { defaultRules } from './defaults/rules.mjs';
@@ -17,6 +17,8 @@ const DEFAULT_TEMPLATE = '@salutejs/canvas-example';
 const checkboxInstructionsTranslation = `\n (Нажмите ${chalk.bold(
     chalk.cyan('<space>'),
 )} чтобы переключить функцию, ${chalk.bold(chalk.cyan('<enter>'))} чтобы завершить выбор)\n`;
+
+class RuntimeError extends Error {}
 
 export async function run() {
     try {
@@ -34,6 +36,10 @@ export async function run() {
 
         if (templatePath) {
             resolvedTemplatePath = path.resolve(templatePath);
+
+            if (!existsSync(path.resolve(resolvedTemplatePath))) {
+                throw new RuntimeError(`create-app: Invalid templatePath '${templatePath}': No such file or directory`);
+            }
         } else {
             const packageBuffer = await downloadTemplatePackage(templatePackage);
             resolvedTemplatePath = extractTemplatePackage(packageBuffer);
@@ -137,6 +143,12 @@ export async function run() {
         console.log(chalk.green('✅ Готово!'));
         console.log('');
     } catch (err) {
+        if (err instanceof RuntimeError) {
+            console.error(err.message);
+
+            return;
+        }
+
         console.error(err);
     } finally {
         cleanUp();
